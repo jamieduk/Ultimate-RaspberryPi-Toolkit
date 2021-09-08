a29c7cfcd5f00ea215e8288f9099d109c98c9be0
Jay <jamied_uk@hotmail.com>
2021-09-08 11:14:27 +0100

    Add files via upload

--- extra.sh
+++ extra.sh
@@ -20,7 +20,7 @@ fi
 # setup menu perms
 #sudo chmod +x *sh
 #
-#clear
+#
 echo -en "\e[92mExtra Menu For RPi"
 echo ""
 echo ""
@@ -36,7 +36,7 @@ echo "Enter your choice: (Anything else to exit)"
 read n
 case $n in
 1)
-clear
+
 #  op 1
 vcgencmd measure_temp
 echo "Press Enter When Ready"
@@ -44,7 +44,7 @@ read Y
 # returning to menu
 bash extra.sh ;;
 2)
-clear
+
 #  op 2
 sudo raspi-config
 echo "Press Enter When Ready"
@@ -52,7 +52,6 @@ read Y
 # returning to menu
 bash extra.sh ;;
 3)
-clear
 #  op 3
 sudo apt update -y && sudo apt upgrade && sudo rpi-update
 # returning to menu
@@ -60,8 +59,6 @@ echo "Press Enter When Ready"
 read Y
 bash extra.sh ;;
 4)
-clear
-#  op 4
 sudo apt update
 sudo apt full-upgrade
 sudo apt install rpi-eeprom
@@ -70,7 +67,6 @@ read Y
 # returning to menu
 bash extra.sh ;;
 5)
-clear
 #  op 5
 raspistill -o image.jpg
 echo "Press Enter When Ready"
@@ -79,7 +75,6 @@ read Y
 bash extra.sh ;;
 # send arg here
 6)
-clear
 raspividyuv -o video.yuv
 echo "Press Enter When Ready"
 read Y
@@ -90,7 +85,6 @@ sudo ufw enable
 echo "Firewall Enabled"
 bash extra.sh ;;
 8)
-clear
 #  op 7
 exit ;;
 esac
